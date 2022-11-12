8fb29182e4acd3423daf4fdef5e6ff1d67462a61
shibuwodai404 <112820035+shibuwodai404@users.noreply.github.com>
2022-11-12 10:57:35 +0800

    Update Q1solution.py

--- Q1solution.py
+++ Q1solution.py
@@ -1,6 +1,20 @@
 def findsize(a,b,c,d)
 
-  for i in range(b)
-    if i = 'S'
+  for i in range(b):
+    if len(i)>len(i+1):
+      b(i) == b(i+1)
+      size == b(i)
+   
+  for j in range(c):
+    if len(j)>len(j+1):
+      d(j) == d(i+1)
+      request == d(j)
+      
+   
+   if size > request:
+      print("yes")
+      
+    else:
+      print("no")
       
     
